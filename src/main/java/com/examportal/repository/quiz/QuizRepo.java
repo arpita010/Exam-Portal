e78dc88ab0ac12488fc9bf2d392dbfc71a39218a
arpitavtech <arpita.mittal@vidyuttech.com>
2024-04-30 12:19:17 +0530

    quiz repo updated

--- src/main/java/com/examportal/repository/quiz/QuizRepo.java
+++ src/main/java/com/examportal/repository/quiz/QuizRepo.java
@@ -2,7 +2,11 @@ package com.examportal.repository.quiz;
 
 import com.examportal.data.entities.Quiz;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuizRepo extends CrudRepository<Quiz, Long> {}
+public interface QuizRepo
+    extends PagingAndSortingRepository<Quiz, Long>, CrudRepository<Quiz, Long> {
+
+}
